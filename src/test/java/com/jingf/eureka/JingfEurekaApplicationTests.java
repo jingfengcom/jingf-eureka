679b6d1cae3caea3b69d523dfc2c7e8c3acdbaa2
jingfeng <jingfeng@ztzd.com>
2022-01-18 17:52:51 +0800

    eureka server配置

---
+++ src/test/java/com/jingf/eureka/JingfEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingf.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingfEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
